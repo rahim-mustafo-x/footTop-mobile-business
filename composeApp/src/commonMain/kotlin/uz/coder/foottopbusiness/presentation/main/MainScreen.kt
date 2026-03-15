8485a1a40569f5cbfbe3618154cf3d1da11245df
rahim-mustafo-x <rahim.mustafo.x@gmail.com>
2026-03-15 21:42:04 +0500

    set-up stadium add

--- composeApp/src/commonMain/kotlin/uz/coder/foottopbusiness/presentation/main/MainScreen.kt
+++ composeApp/src/commonMain/kotlin/uz/coder/foottopbusiness/presentation/main/MainScreen.kt
@@ -84,7 +84,7 @@ fun MainScreen() {
     if (showAddPitch) {
         AddPitchScreen(
             viewModel = addPitchViewModel,
-            onBack = { showAddPitch = false }
+            onBack = { showAddPitch = !showAddPitch }
         )
     } else {
         ModalNavigationDrawer(
