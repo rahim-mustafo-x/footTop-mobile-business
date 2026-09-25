2cc157311892db699da0d239c41dfb0a40704514
j_sobirov <javlonbek.sobirov@uzinfocom>
2026-09-25 17:30:29 +0500

    new

--- composeApp/src/commonMain/kotlin/uz/coder/foottopbusiness/presentation/main/stadium/addstadium/AddStadiumScreen.kt
+++ composeApp/src/commonMain/kotlin/uz/coder/foottopbusiness/presentation/main/stadium/addstadium/AddStadiumScreen.kt
@@ -10,16 +10,12 @@ import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.ArrowBack
-import androidx.compose.material.icons.filled.*
 import androidx.compose.material.icons.outlined.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
--- composeApp/src/commonMain/kotlin/uz/coder/foottopbusiness/presentation/main/stadium/details/StadiumDetailsScreen.kt
+++ composeApp/src/commonMain/kotlin/uz/coder/foottopbusiness/presentation/main/stadium/details/StadiumDetailsScreen.kt
@@ -31,7 +31,6 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
